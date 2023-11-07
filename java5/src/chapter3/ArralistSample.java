e6509f05ae58d09e8e61197f4a1c631867817235
marcoyimyaem <marcoyimyaem@gmail.com>
2023-11-07 13:23:53 +0800

    day 18

--- java5/src/chapter3/ArralistSample.java
+++ java5/src/chapter3/ArralistSample.java
@@ -1,6 +1,7 @@
 package chapter3;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArralistSample {
     public static void main(String[] args) {
@@ -11,10 +12,44 @@ public class ArralistSample {
         aray1.add("orage2");
         aray1.add("orage3");
         aray1.add("orage4");
+        aray1.add("orage");
         ArrayList<Object>  aray2 = new ArrayList<>(aray1);
         aray1.add("orage5");
         System.out.println(aray2.toString());
         System.out.println(aray1);
+
+//        aray1.remove(2); remove index 2
+        aray1.remove("orage");
+
+        System.out.println(aray1);
+        System.out.println(aray1.set(5,"orage6"));
+        System.out.println(aray1);
+        System.out.println("Clearing aray2");
+        System.out.println(aray2);
+        aray2.clear();
+        System.out.println(aray2);
+        List<String> one = new ArrayList<>();
+        List<String> two = new ArrayList<>();
+        System.out.println(one.equals(two)); // true
+        one.add("a"); // [a] *** one [a] != two []
+        System.out.println(one.equals(two)); // false
+        two.add("a"); // [a] *** one [a] == two [a]
+        System.out.println(one.equals(two)); // true
+        one.add("b"); // [a,b] *** one [a,b] != two [a]
+        two.add(0, "b"); // [b,a] *** one [a,b] != two [b,a]
+        System.out.println(one.equals(two)); // false
+
+        Integer a = 1;
+        Double b = 2.5;
+        int z = Integer.parseInt("123");
+        System.out.println(a.compareTo(3));
+        List numberss = new ArrayList<>();
+        numberss.add(1);
+        numberss.add(2);
+        numberss.remove(1);
+        System.out.println(numberss);
+
+
 
 
 
